422e3f2699ec95d2f789ca727c8655b5db7dcbb5
Xiongxin Xiao <Xiongxin.Xiao@ibm.com>
2022-02-15 12:06:22 -0500

    service set up

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -21,7 +21,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {RouterModule, Routes} from '@angular/router';
-import { EntityDataService, EntityDefinitionService, EntityMetadataMap} from '@ngrx/data';
+import { EntityDataModule, EntityDataService, EntityDefinitionService, EntityMetadataMap} from '@ngrx/data';
 import {compareCourses, Course} from './model/course';
 
 import {compareLessons, Lesson} from './model/lesson';
@@ -38,6 +38,10 @@ export const coursesRoutes: Routes = [
     component: CourseComponent
   }
 ];
+
+const EntityMetadata: EntityMetadataMap = {
+  Course: {}
+}
 
 @NgModule({
   imports: [
@@ -78,8 +82,8 @@ export const coursesRoutes: Routes = [
 })
 export class CoursesModule {
 
-  constructor() {
-
+  constructor(private eds: EntityDefinitionService) {
+    eds.registerMetadataMap(EntityMetadata);
   }
 
 
